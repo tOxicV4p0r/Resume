21777b5708a044f7714bd5060a9db7c8628b9933
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-05-17 17:20:16 +0700

    update content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
             <span>About</span>
             <div className="text-gray-400 hover:text-gray-300">
                 <span>
-                    GM, with 4+ years of expertise in ecosystem and personal-use tool creation.I am deeply passionate about the emerging blockchain technology 
+                    GM, with 4+ years of expertise in blockchain ecosystem and personal-use tool creation.I am deeply passionate about the emerging blockchain technology 
                     and its potential to revolutionize the future of the internet.
                     Seeking opportunities to contribute and be a valuable part of the transformative changes happening in this new era.
                 </span>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,10 +3,17 @@ function Contact() {
         <div className="space-y-3">
             <span>Links</span>
             <div className="grid grid-cols-8 gap-y-3">
+
                 <div className="col-span-2 text-gray-500">Github</div>
                 <div className="col-span-6">
                     <div>
                         <a className="underline underline-offset-4 after:content-['_↗']" href="https://github.com/tOxicV4p0r" target="_blank">tOxicV4p0r</a>
+                    </div>
+                </div>
+                <div className="col-span-2 text-gray-500">linkedin</div>
+                <div className="col-span-6">
+                    <div>
+                        <a className="underline underline-offset-4 after:content-['_↗']" href="https://www.linkedin.com/in/kraisorn-deelert-90a06942/" target="_blank">Kraisorn</a>
                     </div>
                 </div>
                 <div className="col-span-2 text-gray-500">Youtube</div>
