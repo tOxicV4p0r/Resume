bcb257229782247eef1571b05512e1d58d547484
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-15 02:47:39 +0700

    add resume

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav() {
                 <img className="h-10 rounded-full" src="https://i.seadn.io/gcs/files/68de9d09c2e406c342f2321fb0294361.gif?auto=format&dpr=1&w=1000" alt="The Potatoz" />
             </div>
             <div className="float-right p-4">
-                <button className="px-2 py-1 bg-gradient-to-r from-pink-500 to-violet-500 text-white text-sm font-semibold rounded-md shadow-lg focus:outline-none">Connect Wallet</button>
+                <a className="px-2 py-1 bg-gradient-to-r from-pink-500 to-violet-500 text-white text-sm font-semibold rounded-md shadow-lg focus:outline-none" href="https://raw.githubusercontent.com/tOxicV4p0r/Resume/main/resume.pdf" target="_blank" rel="noopener noreferrer">Download Resume</a>
             </div>
         </div>
     );
