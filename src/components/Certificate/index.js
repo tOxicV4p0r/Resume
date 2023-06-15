80db19aa59e857d837745ed1c085339c9f591a26
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-15 12:52:13 +0700

    format for data array

--- src/components/Certificate/index.js
+++ src/components/Certificate/index.js
@@ -1,16 +1,57 @@
+import React from "react";
+
+const data = [
+    {
+        year: "June 2023",
+        cerlink: "https://coursera.org/share/ec72454c71270ede2c47fd23e720be2d",
+        title: "Meta Front-End Developer by Meta",
+        descriptions: [],
+        skills: ["Skills : UI/UX design • JavaScript • React • HTML • CSS"],
+    },
+    {
+        year: "June 2023",
+        cerlink: "https://www.coursera.org/professional-certificates/meta-back-end-developer",
+        title: "Meta Back-End Developer by Meta [in progress 4/9]",
+        descriptions: [],
+        skills: ["Skills : Cloud Hosting • REST API • Python • Django • DBMS"],
+    },
+]
+
 function Certificate() {
     return (
         <div className="space-y-3">
             <span>Certification </span>
             <div className="grid grid-cols-8 gap-y-5">
-                <div className="col-span-2 text-gray-500">June 2023</div>
-                <div className="col-span-6">
-                    <div className="flex flex-col space-y-2">
-                        <div><a className="underline underline-offset-4 after:content-['_↗']" href="https://coursera.org/share/ec72454c71270ede2c47fd23e720be2d" target="_blank" rel="noreferrer">Meta Front-End Developer by Meta</a></div>
-                        <div className="text-gray-400 hover:text-gray-300"></div>
-                        <div className="text-gray-500 hover:text-gray-400 rounded-md bg-gray-700 bg-opacity-20 px-2 py-1">Skills : UI/UX design • JavaScript • React • HTML • CSS</div>
-                    </div>
-                </div>
+                {
+                    data.map(({ year, cerlink, title, descriptions, skills }) => (
+                        <React.Fragment key={title}>
+                            <div className="col-span-2 text-gray-500">{year}</div>
+                            <div className="col-span-6">
+                                <div className="flex flex-col space-y-2">
+                                    <div>
+                                        {
+                                            cerlink ?
+                                                <a className="underline underline-offset-4 after:content-['_↗']" href={cerlink} target="_blank" rel="noreferrer">{title}</a>
+                                                :
+                                                <span>{title}</span>
+                                        }
+
+                                    </div>
+                                    {
+                                        descriptions.map((e, i) => (
+                                            <div key={i} className="text-gray-400 hover:text-gray-300">{e}</div>
+                                        ))
+                                    }
+                                    {
+                                        skills.map((e, i) => (
+                                            <div key={i} className="text-gray-500 hover:text-gray-400 rounded-md bg-gray-700 bg-opacity-20 px-2 py-1">{e}</div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </React.Fragment>
+                    ))
+                }
             </div>
         </div>
     )
--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,36 +1,71 @@
+import React from "react";
+
+const data = [
+    {
+        year: "2020-2021",
+        link: "",
+        title: "Fullstack - Sport Analysis System",
+        descriptions: [
+            "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
+            "Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7."
+        ],
+        skills: [
+            "Front : React • Redux-Saga • Antd",
+            "Back : Playwright • Nginx • NodeJS • MongoDB"
+        ],
+    },
+    {
+        year: "2013-2018",
+        link: "",
+        title: "Police Officer",
+        descriptions: [],
+        skills: [],
+    },
+    {
+        year: "2012-2012",
+        link: "",
+        title: "Middleware System Engineer - IBM",
+        descriptions: [
+            "Implemented and on-going support Middleware infrastructure for internal project.",
+        ],
+        skills: [
+            "IBM Websphere • Linux",
+        ],
+    },
+]
+
 function Experience() {
     return (
         <div className="space-y-3">
             <span>Work Experience</span>
             <div className="grid grid-cols-8 gap-y-5">
-                <div className="col-span-2 text-gray-500">2020-2021</div>
-                <div className="col-span-6">
-                    <div className="flex flex-col space-y-2">
-                        <div className="underline underline-offset-4 after:content-['_↗']">
-                            Fullstack - Sport Analysis System
-                        </div>
-                        <div className="text-gray-400 hover:text-gray-300">Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.</div>
-                        <div className="text-gray-400 hover:text-gray-300">Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7.</div>
-                        <div className="text-gray-500 hover:text-gray-400 rounded-md bg-gray-700 bg-opacity-20 px-2 py-1">Front : React • Redux-Saga • Antd</div>
-                        <div className="text-gray-500 hover:text-gray-400 rounded-md bg-gray-700 bg-opacity-20 px-2 py-1">Back : Playwright • Nginx • NodeJS • MongoDB</div>
-                    </div>
-                </div>
-
-                <div className="col-span-2 text-gray-500">2013-2018</div>
-                <div className="col-span-6">
-                    <div className="flex flex-col space-y-2">
-                        <div>Police Officer</div>
-                    </div>
-                </div>
-
-                <div className="col-span-2 text-gray-500">2012-2012</div>
-                <div className="col-span-6">
-                    <div className="flex flex-col space-y-2">
-                        <div>Middleware System Engineer - IBM</div>
-                        <div className="text-gray-400 hover:text-gray-300">Implemented and on-going support Middleware infrastructure for internal project</div>
-                        <div className="text-gray-500 hover:text-gray-400 rounded-md bg-gray-700 bg-opacity-20 px-2 py-1">IBM Websphere • Linux</div>
-                    </div>
-                </div>
+                {
+                    data.map(({ year, link, title, descriptions, skills }) => (
+                        <React.Fragment key={title}>
+                            <div className="col-span-2 text-gray-500">{year}</div>
+                            <div className="col-span-6">
+                                <div className="flex flex-col space-y-2">
+                                    {
+                                        link ?
+                                            <div className="underline underline-offset-4 after:content-['_↗']"><a className="" href={link} target="_blank" rel="noreferrer">{title}</a></div>
+                                            :
+                                            <div>{title}</div>
+                                    }
+                                    {
+                                        descriptions.map((e, i) => (
+                                            <div key={i} className="text-gray-400 hover:text-gray-300">{e}</div>
+                                        ))
+                                    }
+                                    {
+                                        skills.map((e, i) => (
+                                            <div key={i} className="text-gray-500 hover:text-gray-400 rounded-md bg-gray-700 bg-opacity-20 px-2 py-1">{e}</div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </React.Fragment>
+                    ))
+                }
             </div>
         </div>
     );
